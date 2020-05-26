41a789f858350cfd59a25a72f463d61a7bd333ac
gazivoda <gazivodai61@gmail.com>
2020-05-26 09:10:13 +0200

    Debounce/ CSS

--- src/components/github-search-container/index.tsx
+++ src/components/github-search-container/index.tsx
@@ -2,6 +2,7 @@ import gql from 'graphql-tag';
 import React, { Component } from 'react';
 import { withApollo, WithApolloClient } from 'react-apollo';
 import Search from '../search/index';
+import _ from 'lodash';
 
 const REPOSITORY_ATTRIBUTES = gql`
 fragment RepositoryAttributes on Repository { 
@@ -42,41 +43,40 @@ ${USER_ATTRIBUTES}
 `;
 
 type GithubSearchContainerProps = WithApolloClient<GithubSearchContainer>;
-type MyState = { searchText: string, result: {} };
+type MyState = { searchText: string, result: {}, selectedProfile: any, loading: boolean };
 
 class GithubSearchContainer extends Component<GithubSearchContainerProps, MyState>  {
   constructor(props: GithubSearchContainerProps) {
     super(props);
+    this.state = {
+      searchText: '',
+      result: {},
+      selectedProfile: {},
+      loading: false
+    }
   }
 
-  state = {
-    searchText: '',
-    result: {},
-    selectedProfile: {}
-  }
-
-  _executeSearch = async () => {
+  search = async () => {
     const { searchText } = this.state;
     const result = await this.props.client.query({
       query: GET_CURRENT_USER,
       variables: { searchText }
     });
-
-    this.setState({ result: result.data });
+    this.setState({ result: result.data, loading: false });
   }
 
+  executeSearch = _.debounce(this.search, 300);
+
   handleTextChange = (e) => {
-    this.setState({ searchText: e.target.value })
+    this.setState({ searchText: e.target.value, loading: true })
+    this.executeSearch();
   }
 
   render() {
     return (
       <div>
-        <Search onChange={this.handleTextChange} />
+        <Search onChange={this.handleTextChange} loading={this.state.loading} />
         {this.state.searchText}
-        <button
-          onClick={() => this._executeSearch()}
-        >button</button>
         <div>
           {JSON.stringify(this.state.result)}
         </div>
--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,12 +1,21 @@
 import TextField from "@material-ui/core/TextField";
-import React from "react";
+import React, { Fragment } from "react";
+import { LinearProgress } from "@material-ui/core";
 
 const Search = (props) => {
-    return <TextField
-        variant="outlined"
-        type='text'
-        onChange={(e) => props.onChange(e)}
-    />;
+    return (
+        <div className="search-field">
+            <TextField
+                variant="outlined"
+                type='text'
+                label='User Search'
+                className="search-field__input"
+                onChange={(e) => props.onChange(e)}
+            />
+            {props.loading && <LinearProgress className="searc-field__progress" />}
+
+        </div>
+    );
 }
 
 export default Search;
