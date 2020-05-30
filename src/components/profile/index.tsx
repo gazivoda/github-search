eaf6a2362eea8a946b88ea9a502c26bf514b8162
gazivoda <gazivodai61@gmail.com>
2020-05-31 00:20:32 +0200

    additional styling and animation fixed

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -1,7 +1,6 @@
-import { Avatar, Card, Dialog, useTheme, useMediaQuery, DialogTitle, DialogContent, Button, DialogActions, DialogContentText } from "@material-ui/core";
-import React, { useEffect, useState, ReactNode } from "react";
+import { Avatar, Card } from "@material-ui/core";
+import React, { ReactNode, useEffect, useState } from "react";
 import { animated, useSpring } from 'react-spring';
-import FormDialog from "../repositories-modal";
 
 const Profile = (props: {
     id: string,
@@ -13,24 +12,24 @@ const Profile = (props: {
     children: ReactNode,
     onClick: any
 }) => {
-    const [flipped, setFlipped] = useState(false);
+    const [show, setShow] = useState(false);
 
-    const theme = useTheme();
-
-    const { transform, opacity } = useSpring({
-        opacity: flipped ? 1 : 0,
-        transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
-        config: { mass: 5, tension: 950, friction: 80 }
-    });
+    const { opacity } = useSpring(
+        {
+            opacity: show ? 1 : 0,
+            from: { opacity: 0 },
+            config: { mass: 5, tension: 950, friction: 40 }
+        }
+    );
 
     useEffect(() => {
-        setFlipped(true);
+        setShow(true);
     }, [])
 
     return (
         <animated.div
             className="profile-card"
-            style={{ opacity, transform: transform.interpolate(t => `${t} rotateX(180deg)`) }}
+            style={{ opacity }}
         >
             <Card className="profile-card__container" onClick={props.onClick} title={props.login}>
                 <div className="profile-card__container__first-item">
